3f642873300024488c85543272d9a453d16df30e
brightonrolls <125768324+brightonrolls@users.noreply.github.com>
2023-02-19 00:29:49 +0000

    Update setvars.sh

--- setvars.sh
+++ setvars.sh
@@ -1,8 +1,8 @@
 echo "+++ Set variables."
 # ------------------------------------------------------------------------------
-ACCOUNT_SID=your_account_SID
-AUTH_TOKEN=your_account_auth_token
-WORKSPACE_SID=your_TaskRouter_workspace_SID
+ACCOUNT_SID=ACe106fc612c78e783538b24574d84d7ed
+AUTH_TOKEN=cfcc695f3ef5458f40c97417a4485f7a
+WORKSPACE_SID=WScda33f6dc5c07c89f1f4703e1fe869b1
 export ACCOUNT_SID
 export AUTH_TOKEN
 export WORKSPACE_SID
