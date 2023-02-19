19711724c692487aa1eda2784f6a3d101bc310b0
brightonrolls <125768324+brightonrolls@users.noreply.github.com>
2023-02-19 00:43:32 +0000

    Update setvars.sh

--- setvars.sh
+++ setvars.sh
@@ -3,9 +3,11 @@ echo "+++ Set variables."
 ACCOUNT_SID=ACe106fc612c78e783538b24574d84d7ed
 AUTH_TOKEN=cfcc695f3ef5458f40c97417a4485f7a
 WORKSPACE_SID=WScda33f6dc5c07c89f1f4703e1fe869b1
+WORKER_SID=WKed0b86ebb13a9b156e855fb3000d7eb0
 export ACCOUNT_SID
 export AUTH_TOKEN
 export WORKSPACE_SID
+export WORKER_SID
 echo "+ Variables set."
 php echoVars.php
 # ------------------------------------------------------------------------------
